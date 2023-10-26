c13b288794b55cb4c4603e4eaf39a140a6fbb14d
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-10-26 13:11:33 +0300

    Added header and footer component

---
+++ hopeharbour/src/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer(){
+    const footerStyle={
+        height:"400px",
+        backgroundColor:"#E9E9E9"
+    }
+    return(
+        <div className="footer" style={footerStyle}>
+
+        </div>
+    )
+}
+
+export default Footer
---
+++ hopeharbour/src/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Navbar(){
+
+    const navbarStyle={
+        backgroundColor:"#E9E9E9",
+        height:"140px"
+    }
+
+    return (
+        <div className='navbar' style={navbarStyle}>
+
+        </div>
+    )
+}
+
+export default Navbar
