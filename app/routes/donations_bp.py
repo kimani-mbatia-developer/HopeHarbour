055ba87fc06c545a54ad7c462ab269c28b582084
kimaniMbatiaMoringa <135609499+kimaniMbatiaMoringa@users.noreply.github.com>
2023-10-30 14:45:34 +0300

    Delete app directory
    Resetting base

--- app/routes/donations_bp.py
+++
@@ -1,4 +0,0 @@
-from ..extensions import db, Blueprint
-from ..models.users import User
-
-user_bp = Blueprint("user_bp", __name__)
--- app/routes/users_bp.py
+++
@@ -1,5 +0,0 @@
-from ..extensions import db, Blueprint, request, Resource, Api
-from ..models.users import User
-
-users_bp = Blueprint("users_bp", __name__)
-api = Api(users_bp)
