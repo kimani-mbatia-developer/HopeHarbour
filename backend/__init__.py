d885076fedf717d13ddea84c8e08b2a116a7caac
duan spot <brandennjonge@gmail.com>
2023-11-08 12:36:13 +0300

    Feat: One time donation route

--- backend/__init__.py
+++ backend/__init__.py
@@ -80,6 +80,7 @@ from backend.routes.donations_route import donations_bp, donations_ns
 from backend.routes.applications_route import applications_bp, applications_ns
 from backend.routes.beneficiary_route import beneficiaries_bp, beneficiaries_ns
 from backend.routes.email_route import email
+from backend.routes.paid_route import onetimepay
 # from backend.routes.payments_route import payments_bp, payments_ns
 from backend.routes.user_route import users_bp, users_ns
 
@@ -93,6 +94,7 @@ app.register_blueprint(donations_bp)
 app.register_blueprint(applications_bp)
 app.register_blueprint(beneficiaries_bp)
 app.register_blueprint(stories_bp)
+app.register_blueprint(onetimepay)
 # app.register_blueprint(payments_bp)
 app.register_blueprint(users_bp)
 
